e62225af568ed9e89926dd246ab3c3a496d31c66
ПК <svetochka116236@gmail.com>
2023-12-15 10:31:30 +0200

    added condition theory

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-
+# Користувач вводить три цифри з клавіатури.
+# Залежно від вибору користувача програма виводить на екран максимум із трьох,
+# мінімум із трьох або середньоарифметичне трьох чисел.
 #
+
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+#
+# choice = int(input("Enter your choice (1/2/3): "))
+#
+# if choice == 1:
+#     maximum = max(n1, n2, n3)
+#     print("Maximum of the three numbers is:", maximum)
+# elif choice == 2:
+#     minimum = min(n1, n2, n3)
+#     print("Minimum of the three numbers is:", minimum)
+# elif choice == 3:
+#     average = (n1 + n2 + n3) / 3
+#     print("Average of the three numbers is:", average)
+# else:
+#     print("Invalid choice")

75e009e5894de817a1b91da0f1c5ae50095f21fc
ПК <svetochka116236@gmail.com>
2023-12-19 21:36:53 +0200

    implemented second task

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-# Користувач вводить два числа.
-# Визначити, чи рівні ці числа, і, якщо ні,
-# вивести їх на екран у порядку зростання
+# Користувач вводить два числа та матем дію: + - * або /
+#
+# Залежно від введеної матем дії вивести результат
 
-
-while True:
-    try:
-        num1 = float(input("Введіть перше число: "))
-        num2 = float(input("Введіть друге число: "))
-
-        if num1 == num2:
-            print("Числа рівні.")
-        else:
-            sorted_numbers = sorted([num1, num2])
-            print("Числа у порядку зростання:", sorted_numbers)
-
-    except ValueError:
-        print("Невірне введення. Будь ласка, введіть числа.")
+#
+# while True:
+#     try:
+#         num1 = float(input("Введіть перше число: "))
+#         num2 = float(input("Введіть друге число: "))
+#         operation = input("Введіть операцію (+, -, *, /): ")
+#
+#         if operation == '+':
+#             result = num1 + num2
+#         elif operation == '-':
+#             result = num1 - num2
+#         elif operation == '*':
+#             result = num1 * num2
+#         elif operation == '/':
+#             if num2 == 0:
+#                 result = "Помилка: ділення на нуль"
+#             else:
+#                 result = num1 / num2
+#         else:
+#             result = "Невірна операція"
+#
+#         print("Результат:", result)
+#
+#     except ValueError:
+#         print("Невірне введення. Будь ласка, введіть числа.")

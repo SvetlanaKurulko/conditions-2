3a21d30ae62cc25efbda950154b26be1b6f11b0f
ПК <svetochka116236@gmail.com>
2023-12-19 21:25:29 +0200

    implemented second task

--- main.py
+++ main.py
@@ -1,51 +1,18 @@
-# Користувач вводить три цифри з клавіатури.
-# Залежно від вибору користувача програма виводить на екран максимум із трьох,
-# мінімум із трьох або середньоарифметичне трьох чисел.
-#
+# Користувач вводить два числа.
+# Визначити, чи рівні ці числа, і, якщо ні,
+# вивести їх на екран у порядку зростання
 
 
-# n1 = int(input("Enter first number: "))
-# n2 = int(input("Enter second number: "))
-# n3 = int(input("Enter third number: "))
-#
-# choice = int(input("Enter your choice (1/2/3): "))
-#
-# if choice == 1:
-#     maximum = max(n1, n2, n3)
-#     print("Maximum of the three numbers is:", maximum)
-# elif choice == 2:
-#     minimum = min(n1, n2, n3)
-#     print("Minimum of the three numbers is:", minimum)
-# elif choice == 3:
-#     average = (n1 + n2 + n3) / 3
-#     print("Average of the three numbers is:", average)
-# else:
-#     print("Invalid choice")
+while True:
+    try:
+        num1 = float(input("Введіть перше число: "))
+        num2 = float(input("Введіть друге число: "))
 
+        if num1 == num2:
+            print("Числа рівні.")
+        else:
+            sorted_numbers = sorted([num1, num2])
+            print("Числа у порядку зростання:", sorted_numbers)
 
-
-######
-
-# Користувач вводить з клавіатури кількість метрів.
-# Залежно від вибору користувача програма переводить метри милі, дюйми або ярди.
-
-# meters = float(input("Введіть кількість метрів: "))
-#
-# print("Оберіть одиницю виміру для переведення:")
-# print("1. Метри в милі")
-# print("2. Метри в дюйми")
-# print("3. Метри в ярди")
-#
-# choice = int(input("Ваш вибір (1/2/3): "))
-#
-# if choice == 1:
-#     miles = meters * 0.000621371
-#     print(f"{meters} метрів дорівнює {miles} миль")
-# elif choice == 2:
-#     inches = meters * 39.3701
-#     print(f"{meters} метрів дорівнює {inches} дюймів")
-# elif choice == 3:
-#     yards = meters * 1.09361
-#     print(f"{meters} метрів дорівнює {yards} ярдів")
-# else:
-#     print("Невірний вибір")
+    except ValueError:
+        print("Невірне введення. Будь ласка, введіть числа.")
